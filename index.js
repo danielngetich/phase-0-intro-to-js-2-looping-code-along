be7489673443c1df9cc7ff422c65dd60e7eb8232
ngetich kimutai daniel <daniel.ngetich@student.moringaschool.com>
2022-08-12 12:11:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const sinon = [];
+var out=[];
+function writeCards(sinon,event) {
+  for (let i = 0; i < sinon.length; i++) {
+   out.push(`Thank you, ${sinon[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return out;
+}
+function countDown(a){
+  while (a>=0){
+    console.log(a--)
+  }
+}
+
+
